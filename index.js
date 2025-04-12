a45afff6d2eaabaf40f4a7df82771ede623f732d
JohnPhann <openaigptsmart@gmail.com>
2025-04-12 10:44:46 +0700

    Update restfull api

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb+srv://nghiapchpo:123123123@cluster0.ohklk5c.mongodb.ne
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use(session({
     secret: "your-secret-key",  // Change to a strong secret key
     resave: false,
@@ -31,6 +32,7 @@ app.set('view engine', 'ejs');
 // Import router
 const homeRoutes = require('./src/routes/homeRoutes');
 const userRoutes = require('./src/routes/userRoutes'); 
+const userRoutesApi = require('./src/routes/api/user'); 
 const productRoutes = require('./src/routes/productRoutes');
 const bookingRoutes = require('./src/routes/bookingRoutes');
 const categoryRoutes = require('./src/routes/categoryRoutes');
@@ -42,6 +44,7 @@ app.use('/bookings', bookingRoutes);
 app.use('/category', categoryRoutes);
 
 
+app.use('/api/users', userRoutesApi);
 
 // Chạy server
 app.listen(PORT, () => {
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    gender:String,
+    role:String,
+    phone:String
 });
 
 module.exports = mongoose.model('User', userSchema);
